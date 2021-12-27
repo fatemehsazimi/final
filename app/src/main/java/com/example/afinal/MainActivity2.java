ee0499433d2d3883ae78628cca3330ede93181a8
Admin <fatemehazimi200288@gmail.com>
2021-12-28 01:58:21 +0330

    .

--- app/src/main/java/com/example/afinal/MainActivity2.java
+++ app/src/main/java/com/example/afinal/MainActivity2.java
@@ -2,15 +2,55 @@ package com.example.afinal;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
-public class MainActivity2 extends AppCompatActivity {
-
+public class MainActivity2 extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+    private Spinner spinner;
+    private Spinner spinner2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
 
         getSupportActionBar().hide();
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent=new Intent(MainActivity2.this,MainActivity3.class);
+                startActivity(intent);
+                finish();
+            }
+        },8000);
+
+        spinner=findViewById(R.id.spinner1);
+        spinner2=findViewById(R.id.spinner2);
+        ArrayAdapter<CharSequence> spinneradapter=ArrayAdapter.createFromResource(this,R.array.language, android.R.layout.simple_spinner_item);
+        ArrayAdapter<CharSequence>spinneradapter2=ArrayAdapter.createFromResource(this,R.array.language, android.R.layout.simple_spinner_item);
+        spinneradapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinneradapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(spinneradapter);
+        spinner2.setAdapter(spinneradapter2);
+
+        spinner.setOnItemSelectedListener(this);
+        spinner2.setOnItemSelectedListener(this);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long id) {
+        String choice = adapterView.getItemAtPosition(i).toString();
+        Toast.makeText(getApplicationContext(),choice,Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
     }
 }
